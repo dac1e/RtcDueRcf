c02126edf7e75c8cf43ce6ceb44191fe0291f784
dac1e <wolfgang@die-schmieders.de>
2025-01-30 19:22:59 +0100

    Change copyright notes.

--- src/internal/core-sam-GapClose.h
+++ src/internal/core-sam-GapClose.h
@@ -1,9 +1,26 @@
 /*
- * core-sam-GapClose.h
- *
- *  Created on: 07.01.2025
- *      Author: Wolfgang
- */
+  RtcSam3XA - Arduino libary for RtcSam3XA - builtin RTC Copyright (c)
+  2024 Wolfgang Schmieder.  All right reserved.
+
+  Contributors:
+  - Wolfgang Schmieder
+
+  Project home: https://github.com/dac1e/RcSwitchReceiver/
+
+  This library is free software; you can redistribute it and/or modify it
+  the terms of the GNU Lesser General Public License as under published
+  by the Free Software Foundation; either version 3.0 of the License,
+  or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+*/
 
 #pragma once
 
